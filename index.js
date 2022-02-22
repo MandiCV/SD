0156a9b56a40f5f07d833119641a97ccfa555569
MandiCV <amandacascales@gmail.com>
2022-02-22 17:38:45 +0100

    agregando morgan y loggeral repositorio

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict'
 
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 3000;
 const express = require('express');
+const logger = require('morgan');
 const app = express();
+
+// Declaro los middleware
+app.use(logger('dev'));
 
 app.get('/hola/:unNombre', (req, res) => {
     res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` });
