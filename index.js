3b94b908629528c773a78fbe1acbe4fda2272bc8
MandiCV <amandacascales@gmail.com>
2022-03-02 15:37:09 +0100

    API REST CRUD (sin base de datos)

--- index.js
+++ index.js
@@ -2,16 +2,68 @@
 
 const port = process.env.PORT || 3000;
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const logger = require('morgan');
 const app = express();
 
 // Declaro los middleware
 app.use(logger('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.get('/hola/:unNombre', (req, res) => {
-    res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` });
+// Declaramos nuestras rutas, controladores y logica de negocioc
+
+app.get('/api/product', getProductsController);
+
+function getProductsController(req, res){
+    //Aqui vendria la logica de negocio
+
+    res.status(200).send({
+        msg:"Ahí va la tabla productos solicitada",
+        productos:[]
+    });
+}
+
+app.get('/api/product/:productID', (req, res)=>{
+    const id = req.params.productID;
+            //Aqui vendria la logica de negocio
+    res.status(200).send({
+        msg: `Ahí va el producto ${id} solicitado`
+    });
 });
 
+app.post('/api/product/', (req, res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+
+        //Aqui vendria la logica de negocio
+
+    res.status(200).send({
+        msg: `El producto se ha recibido`
+    });
+
+});
+
+app.put('/api/product/:productID', (req, res)=>{
+    const id = req.params.productID;
+    const newData = req.body;
+
+            //Aqui vendria la logica de negocio
+
+    res.status(200).send({
+        productos: `${req.params.productID}`,
+        nuevoProducto: newData
+    });
+
+});
+app.delete('/api/product/:productID', (req, res)=> {
+    res.status(200).send({
+        productos: `${req.params.productID}`
+    });
+});
+
+
 app.listen(port, () => {
-    console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
+    console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
 });
