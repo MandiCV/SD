4c6bfe9c24d9bab96eed8a5b93aeb0425ea3a3e4
MandiCV <amandacascales@gmail.com>
2022-03-03 01:16:02 +0100

    API REST CRUD con MongoDB

--- index.js
+++ index.js
@@ -1,69 +1,100 @@
 'use strict'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+
 const express = require('express');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
 const logger = require('morgan');
+const mongojs = require('mongojs');
+
+
 const app = express();
 
-// Declaro los middleware
+var db = mongojs("SD");
+var id = mongojs.ObjectID;
+
+
+//middlewares
+
 app.use(logger('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// Declaramos nuestras rutas, controladores y logica de negocioc
+//añadimos el trigger previo para dar soporte a múltiples colecciones
 
-app.get('/api/product', getProductsController);
+app.param("coleccion", (req, res, next, coleccion) =>{
+   console.log('param /api/:coleccion');
+   console.log('colección: ', coleccion);
+   
+    req.collection = db.collection(coleccion);
+    return next();
+});
 
-function getProductsController(req, res){
-    //Aqui vendria la logica de negocio
+//rutas
 
-    res.status(200).send({
-        msg:"Ahí va la tabla productos solicitada",
-        productos:[]
-    });
-}
+app.get('/api', (req, res, next) =>{
+    console.log('GET /api');
+    console.log(req.params);
+    console.log(req.collection);
 
-app.get('/api/product/:productID', (req, res)=>{
-    const id = req.params.productID;
-            //Aqui vendria la logica de negocio
-    res.status(200).send({
-        msg: `Ahí va el producto ${id} solicitado`
+    db.getCollectionNames((err, colecciones) =>{
+        if (err) return next(err);
+        res.json(colecciones);
     });
 });
 
-app.post('/api/product/', (req, res)=>{
-    const newProduct = req.body;
-    console.log(newProduct);
+app.get('/api/:coleccion', (req, res, next) =>{
 
-        //Aqui vendria la logica de negocio
-
-    res.status(200).send({
-        msg: `El producto se ha recibido`
-    });
-
-});
-
-app.put('/api/product/:productID', (req, res)=>{
-    const id = req.params.productID;
-    const newData = req.body;
-
-            //Aqui vendria la logica de negocio
-
-    res.status(200).send({
-        productos: `${req.params.productID}`,
-        nuevoProducto: newData
-    });
-
-});
-app.delete('/api/product/:productID', (req, res)=> {
-    res.status(200).send({
-        productos: `${req.params.productID}`
+    req.collection.find((err, coleccion) =>{
+        if (err) return next(err);
+        res.json(coleccion);
     });
 });
 
+app.get('/api/:coleccion/:id', (req, res, next) =>{
 
+    req.collection.findOne({_id: id(req.params.id)},(err, elemento) =>{
+        if (err) return next(err);
+        res.json(elemento);
+    });
+});
+
+app.post('/api/:coleccion', (req, res, next) =>{
+    const elemento = req.body;
+
+    if(!elemento.nombre) {
+        res.status(400).json ({
+            error: 'Bad Data',
+            description: 'Se precisa al menos un campo <nombre>'
+        });
+    } else {
+        req.collection.save(elemento, (err, coleccionGuardada) => {
+            if (err) return next(err);
+            res.json(coleccionGuardada);
+        });
+    }
+});
+
+app.put('/api/:coleccion/:id', (req, res, next) => {
+    let elementoId = req.params.id;
+    let elementoNuevo = req.body;
+    req.collection.update({_id: id(elementoId)},
+        {$set: elementoNuevo}, {sasfe: true, multi: false}, (err,elementoModif) => {
+            if(err) return next(err);
+            res.json(elementoModif);
+        });
+});
+
+app.delete('/api/:coleccion/:id', (req, res, next) => {
+    let elementoId = req.params.id;
+
+    req.collection.remove({_id: id(elementoId)}, (err, resultado) =>{
+        if (err) return next(err);
+        res.json(resultado);
+    });
+});
+
+//iniciamos la app
 app.listen(port, () => {
-    console.log(`API REST ejecutándose en http://localhost:${port}/api/product`);
+    console.log(`API REST ejecutándose en http://localhost:${port}/api/:coleccion/:id`);
+    
 });
